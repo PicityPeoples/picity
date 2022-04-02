13736d19272ee7cfa475aac40f0a4a51c1e6fd5f
PicityPeoples <102811154+PicityPeoples@users.noreply.github.com>
2022-04-02 06:05:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ app.use((req, res, next) => {
   next()
 })
 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }))
+app.use(cors({ origin: "https://pictpeoples.herokuapp.com/", credentials: true }))
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
