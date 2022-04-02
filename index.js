c4677279231377777261cb7e55de12698573271b
Charlie0560 <chaitanyalokhande87@gmail.com>
2022-04-02 07:17:13 +0530

    changes9

--- index.js
+++ index.js
@@ -18,35 +18,47 @@ const multer = require("multer");
 const path = require("path");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const mailgun = require("mailgun-js");
+const DOMAIN = "sandbox9c3d5b46ad5f4e20bc359a158f356f92.mailgun.org";
+const mg = mailgun({
+  apiKey: "4c31c759fe6e23f16f8501c26c0adada-e2e3d8ec-cd60d513",
+  domain: DOMAIN,
+});
+
+dotenv.config();
+
+try {
+  mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {
+    console.log("Connected to MONGODB");
+  });
+} catch (err) {
+  console.log(err);
+}
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested, Content-Type, Accept Authorization"
-  )
+  );
   if (req.method === "OPTIONS") {
-    res.header(
-      "Access-Control-Allow-Methods",
-      "POST, PUT, PATCH, GET, DELETE"
-    )
-    return res.status(200).json({})
+    res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE");
+    return res.status(200).json({});
   }
-  next()
-})
+  next();
+});
 
 // app.use(cors({ origin: "https://pictpeoples.herokuapp.com/", credentials: true }))
-var whitelist = ['https://pictpeoples.herokuapp.com/']
+var whitelist = ["https://pictpeoples.herokuapp.com/"];
 var corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
+      callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'))
+      callback(new Error("Not allowed by CORS"));
     }
-  }
-}
-
+  },
+};
 
 app.use(express.json());
 app.use(helmet());
@@ -64,7 +76,6 @@ app.use("/api/clubupdates", clubsupdateRoute);
 app.use("/images", express.static(path.join(__dirname, "/images")));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -100,13 +111,13 @@ app.post("/sendmail", cors(), async (req, res) => {
     subject: "Email Verfication",
     text: `${text}`,
   };
-  try{
+  try {
     mg.messages().send(data, function (error, body) {
       console.log(error);
       console.log(data);
-      console.log("Sent successfully")
+      console.log("Sent successfully");
     });
-  }catch(err){
+  } catch (err) {
     console.log(err);
   }
 
@@ -131,9 +142,9 @@ app.post("/sendmail", cors(), async (req, res) => {
   //   console.log(err);
   // }
 });
-app.get('/',cors(corsOptions),(req,res)=>{
+app.get("/", cors(corsOptions), (req, res, next) => {
   res.send("App is running");
-})
-app.listen(process.env.PORT || 5000 , () => {
+});
+app.listen(process.env.PORT || 5000, () => {
   console.log("Backend is running!");
 });
